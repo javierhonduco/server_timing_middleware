d4ecec9e1e33b7233776f24e74f2a46f5badca5d
Javier Honduvilla Coto <javierhonduco@gmail.com>
2017-04-04 15:46:19 +0200

    first commit

---
+++ lib/server_timing_middleware/version.rb
@@ -0,0 +1,3 @@
+module ServerTimingMiddleware
+  VERSION = "0.0.1"
+end
---
+++ test/server_timing_middleware_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'active_support/notifications'
+
+# TODO(javierhonduco): test the proper addition of events
+# with the same name
+class ServerTimingMiddlewareTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ServerTimingMiddleware::VERSION
+  end
+
+  def test_no_notifications
+    status, headers, body = Rack::ServerTimingMiddleware.new(FakeRack.new).call(nil)
+    server_timing = headers['Server-Timing']
+
+    refute_nil server_timing
+    assert_empty server_timing
+  end
+
+  def test_multiple_notifications_simple
+    status, headers, body = Rack::ServerTimingMiddleware.new(SimpleMockedRack.new).call(nil)
+    server_timing = headers['Server-Timing']
+
+    assert_match /omg/, server_timing
+    assert_match /lol/, server_timing
+    assert_match /kawaii/, server_timing
+  end
+end
